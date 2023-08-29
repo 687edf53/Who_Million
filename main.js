16e164f1f53dca379d1803eba9cc6f454482b8cb
687edf53 <sm3257698@gmail.com>
2023-08-29 20:50:42 +0200

    Some Improvments

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ window.addEventListener("load", () => {
   setTimeout(() => {
     StartingAnimation.style.display = "none";
     // End Animation
-    adiyaSection.style.display = "block";
+    adiyaSection.style.display = "flex";
   }, 3750);
 
   // Start Button and adiya
@@ -37,3 +37,91 @@ Start_Button.addEventListener("click", () => {
 });
 
 // End Game Section
+
+// newUser Section
+
+const newUserButton = document.getElementById('newUser');
+const newUserForm = document.getElementById('newUserForm');
+const newUserName = document.getElementById('userName')
+const addUserButton = document.querySelector('#newUserForm > input[type=\'submit\']')
+
+const past_levels = document.querySelector('.past-levels')
+
+newUserButton.addEventListener('click', () => {
+  newUserForm.style.display = `flex`;
+})
+
+addUserButton.addEventListener('click', () => {
+  if(newUserName.value != '') {
+    let obj = {
+      title: newUserName.value,
+      id: new Date(),
+      exist: false
+    }
+    localStorage.setItem("user", JSON.stringify(obj));
+    newUserForm.style.display = "none";
+    newUserButton.style.display = "none";
+    past_levels.classList.remove('none')
+  }
+  
+})
+
+// Start Magic Buttons
+const magic_button = document.querySelectorAll(".btn-sq2").forEach((btn) => {
+  btn.addEventListener("mousemove", (e) => {
+    // place of mouse in x
+    let x = e.offsetX;
+    // place of mouse in y
+    let y = e.offsetY;
+    // width of button
+    let btnWidth = btn.clientWidth;
+    // height of button
+    let btnHeight = btn.clientHeight;
+    // mouse in x - button width / 2
+    let transX = (x - btnWidth / 2);
+    // mouse in y - button height / 2
+    let transY = (y - btnHeight / 2);
+    btn.style.transform = `translateX(${transX}px)  translateY(${transY}px)`;
+
+
+    let mx = e.pageX - btn.offsetLeft;
+    let my = e.pageY - btn.offsetTop;
+    btn.style.setProperty('--x', mx + 'px')
+    btn.style.setProperty('--y', my + 'px')
+  });
+
+  btn.addEventListener('mouseout', (e) => {
+    btn.style.transform = ''
+  })
+
+  
+});
+magic_button.forEach((btn) => {
+  btn.addEventListener("touchmove", (e) => {
+    e.preventDefault(); // Prevent default touch action
+
+    // Touch position relative to the button
+    let touch = e.touches[0];
+    let x = touch.clientX - btn.getBoundingClientRect().left;
+    let y = touch.clientY - btn.getBoundingClientRect().top;
+
+    // Button dimensions
+    let btnWidth = btn.clientWidth;
+    let btnHeight = btn.clientHeight;
+
+    // Calculate translations
+    let transX = x - btnWidth / 2;
+    let transY = y - btnHeight / 2;
+
+    btn.style.transform = `translateX(${transX}px) translateY(${transY}px)`;
+
+    // Set custom properties for animation (CSS Variables)
+    btn.style.setProperty("--x", x + "px");
+    btn.style.setProperty("--y", y + "px");
+  });
+
+  btn.addEventListener("touchend", () => {
+    btn.style.transform = "";
+  });
+});
+// End Magic Buttons
